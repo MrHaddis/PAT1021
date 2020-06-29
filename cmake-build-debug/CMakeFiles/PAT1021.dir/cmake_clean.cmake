80b80e38a654b4a01e417961252d9d1ac1a5a9b0
duanyuxiang <duanyx@belink.com>
2020-06-29 16:42:28 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/PAT1021.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PAT1021.dir/main.c.obj"
+  "PAT1021.exe"
+  "PAT1021.exe.manifest"
+  "PAT1021.pdb"
+  "libPAT1021.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PAT1021.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/PAT1021.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for PAT1021.
+# This may be replaced when dependencies are built.
